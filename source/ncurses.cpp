af714444aa9d55745fd0ac54cb968862251e2160
grncbg <grncbg@gmail.com>
2017-05-25 14:29:12 +0900

    Add コメントの追加

---
+++ source/ncurses.cpp
@@ -0,0 +1,12 @@
+/*
+	ncurses.cpp
+	Ncursesクラスの実装
+
+	Copyright (c) 2017 KogCoder
+
+	This software is released under the MIT License.
+	http://opensource.org/licenses/mit-license.php
+
+	Auther : teapot
+	E-mail : grncbg@gmail.com
+*/
