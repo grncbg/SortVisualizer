09989b7a2fa8fa27a14b3f6f5176a7ff9c3a44c2
grncbg <grncbg@gmail.com>
2017-05-25 13:12:06 +0900

    Add main.cpp

---
+++ source/main.cpp
@@ -0,0 +1,15 @@
+/*
+	main.cpp
+
+	Copyright (c) 2017 KogCoder
+
+	This software is released under the MIT License.
+	http://opensource.org/licenses/mit-license.php
+
+	Auther : teapot
+*/
+
+// main関数
+int main( void ){
+	return 0;
+}
