67c9897ab4cf5ee788db3f1bcebec71b23cba462
grncbg <grncbg@gmail.com>
2017-05-25 14:29:54 +0900

    Add コメントの追加

--- include/ncurses.hpp
+++ include/ncurses.hpp
@@ -1,3 +1,16 @@
+/*
+	ncurses.hpp
+	Ncursesクラス
+
+	Copyright (c) 2017 KogCoder
+
+	This software is released under the MIT License.
+	http://opensource.org/licenses/mit-license.php
+
+	Auther : teapot
+	E-mail : grncbg@gmail.com
+*/
+
 #ifndef INCLUDED_NCURSES_HPP
 #define INCLUDED_NCURSES_HPP
 
