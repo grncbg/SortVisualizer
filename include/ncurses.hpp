253efac6291ca3a094ef5bd136c118ea7af60594
grncbg <grncbg@gmail.com>
2017-05-26 15:42:43 +0900

    Update tabからspace

--- include/ncurses.hpp
+++ include/ncurses.hpp
@@ -1,13 +1,13 @@
 /*
-	ncurses.hpp
-	Ncursesクラス
+    ncurses.hpp
+    Ncursesクラス
 
-	Copyright (c) 2017 KogCoder
+    Copyright (c) 2017 KogCoder
 
-	This software is released under the MIT License.
-	http://opensource.org/licenses/mit-license.php
+    This software is released under the MIT License.
+    http://opensource.org/licenses/mit-license.php
 
-	Auther : teapot <grncbg@gmail.com>
+    Auther : teapot <grncbg@gmail.com>
 */
 
 #include <string>
@@ -21,62 +21,62 @@
 
 namespace kogcoder{
 
-	/*
-		class Ncurses
-			ncursesの薄いラッパー
-		コンストラクタ
-			Ncurses()
-			Ncurses ( Ncurses& ) = delete
-		デストラクタ
-			~Ncurses()
-		関数
-			int printw ( const char*, Args const & ... )
-			int printw ( const std::string, Args const & ... )
-			int refresh (void)
-			int getch (void)
-	*/
-	class Ncurses{
-	public:
-		Ncurses ();
-		Ncurses ( Ncurses& ) = delete;
-		~Ncurses ();
+    /*
+        class Ncurses
+            ncursesの薄いラッパー
+        コンストラクタ
+            Ncurses()
+            Ncurses ( Ncurses& ) = delete
+        デストラクタ
+            ~Ncurses()
+        関数
+            int printw ( const char*, Args const & ... )
+            int printw ( const std::string, Args const & ... )
+            int refresh (void)
+            int getch (void)
+    */
+    class Ncurses{
+    public:
+        Ncurses ();
+        Ncurses ( Ncurses& ) = delete;
+        ~Ncurses ();
 
-		template < class ... Args >
-		int printw ( const char*, Args const & ... );
-		template < class ... Args >
-		int printw ( const std::string &, Args const & ... );
+        template < class ... Args >
+        int printw ( const char*, Args const & ... );
+        template < class ... Args >
+        int printw ( const std::string &, Args const & ... );
 
-		template < class ... Args >
-		int mvprintw ( const int, const int, const char*, Args const & ...);
-		template < class ... Args >
-		int mvprintw ( const int, const int, const std::string, Args const & ...);
+        template < class ... Args >
+        int mvprintw ( const int, const int, const char*, Args const & ...);
+        template < class ... Args >
+        int mvprintw ( const int, const int, const std::string, Args const & ...);
 
-		int refresh( void );
+        int refresh( void );
 
-		int getch( void );
+        int getch( void );
 
-		int clear( void );
-	};
+        int clear( void );
+    };
 
 }
 
 template < class ... Args >
 int kogcoder::Ncurses::printw ( const char* format, Args const & ... args ){
-	return ::printw( format, args ... );
+    return ::printw( format, args ... );
 };
 
 template < class ... Args >
 int kogcoder::Ncurses::printw ( const std::string & format, Args const & ... args ){
-	return printw( format.c_str(), args ... );
+    return printw( format.c_str(), args ... );
 };
 
 template < class ... Args >
 int kogcoder::Ncurses::mvprintw ( const int y, const int x, const char* format, Args const & ... args ){
-	return ::mvprintw( y, x, format, args ... );
+    return ::mvprintw( y, x, format, args ... );
 }
 template < class ... Args >
 int kogcoder::Ncurses::mvprintw ( const int y, const int x, const std::string format, Args const & ... args ){
-	return mvprintw( y, x, format.c_str(), args ... );
+    return mvprintw( y, x, format.c_str(), args ... );
 }
 
 #endif
--- source/ncurses.cpp
+++ source/ncurses.cpp
@@ -1,41 +1,41 @@
 /*
-	ncurses.cpp
-	Ncursesクラスの実装
+    ncurses.cpp
+    Ncursesクラスの実装
 
-	Copyright (c) 2017 KogCoder
+    Copyright (c) 2017 KogCoder
 
-	This software is released under the MIT License.
-	http://opensource.org/licenses/mit-license.php
+    This software is released under the MIT License.
+    http://opensource.org/licenses/mit-license.php
 
-	Auther : teapot <grncbg@gmail.com>
+    Auther : teapot <grncbg@gmail.com>
 */
 
 #include "ncurses.hpp"
 
 namespace kogcoder{
 
-	Ncurses::Ncurses(){
+    Ncurses::Ncurses(){
 
-		initscr();
+        initscr();
 
-	}
+    }
 
-	Ncurses::~Ncurses(){
+    Ncurses::~Ncurses(){
 
-		endwin();
+        endwin();
 
-	}
+    }
 
-	int Ncurses::refresh(){
-		return ::refresh();
-	}
+    int Ncurses::refresh(){
+        return ::refresh();
+    }
 
-	int Ncurses::getch( void ){
-		return ::getch();
-	}
+    int Ncurses::getch( void ){
+        return ::getch();
+    }
 
-	int Ncurses::clear( void ){
-		return ::clear();
-	}
+    int Ncurses::clear( void ){
+        return ::clear();
+    }
 
 }
