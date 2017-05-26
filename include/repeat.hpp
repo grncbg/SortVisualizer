903e2f6f36e9e9dd9e76204efdbc418aead68204
grncbg <grncbg@gmail.com>
2017-05-26 23:02:36 +0900

    Update コメントの追加

---
+++ include/repeat.hpp
@@ -0,0 +1,11 @@
+/*
+    repeat.hpp
+    Repeatクラス
+
+    Copyright (c) 2017 KogCoder
+
+    This software is released under the MIT License.
+    http://opensource.org/licenses/mit-license.php
+
+    Auther : teapot <grncbg@gmail.com>
+*/
