2826e4bb51957b4d3191d9c88382167e7f39c7a3
grncbg <grncbg@gmail.com>
2017-05-28 20:08:21 +0900

    Add visualizerの追加

---
+++ source/visualizer.cpp
@@ -0,0 +1,11 @@
+/*
+    visualizer.cpp
+    ビジュアライザクラス
+
+    Copyright (c) 2017 KogCoder
+
+    This software is released under the MIT License.
+    http://opensource.org/licenses/mit-license.php
+
+    Auther : teapot <grncbg@gmail.com>
+*/
