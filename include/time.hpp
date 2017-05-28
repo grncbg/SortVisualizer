a472ddbc3480a19217e96d4daa64a33afa3ce1f2
grncbg <grncbg@gmail.com>
2017-05-28 16:23:59 +0900

    Add wait関数の追加

--- include/time.hpp
+++ include/time.hpp
@@ -13,11 +13,23 @@
 #ifndef INCLUDED_TIMT_HPP
 #define INCLUDED_TIMT_HPP
 
+#include <chrono>
+
 namespace kogcoder{
 
     class Time{
-
+        template < class T = std::milli >
+        static void wait(std::chrono::duration< long long, T > &&);
     };
+
+    template < class T >
+    void Time::wait(std::chrono::duration< long long, T > &&time){
+        std::chrono::steady_clock::time_point s,e;
+        s = std::chrono::steady_clock::now();
+        e = std::chrono::steady_clock::now();
+        while( e - s < time )
+            e = std::chrono::steady_clock::now();
+    }
 
 }
 
