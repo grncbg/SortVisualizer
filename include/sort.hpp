777298869669e3abb3c093d7174f51fff4e8fe6c
grncbg <grncbg@gmail.com>
2017-05-28 17:00:37 +0900

    Add sortの追加

---
+++ include/sort.hpp
@@ -0,0 +1,11 @@
+/*
+    sort.hpp
+    各種Sortクラス
+
+    Copyright (c) 2017 KogCoder
+
+    This software is released under the MIT License.
+    http://opensource.org/licenses/mit-license.php
+
+    Auther : teapot <grncbg@gmail.com>
+*/
