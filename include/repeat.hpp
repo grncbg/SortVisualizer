eae57e885e9704f3a8905a8f4063e42db815acf0
grncbg <grncbg@gmail.com>
2017-06-07 08:55:15 +0900

    Update

--- include/repeat.hpp
+++ include/repeat.hpp
@@ -42,14 +42,30 @@ namespace kogcoder{
         virtual int run ( void ) = 0;
     };
 
+    /*
+        class Repeat
+            概要
+                指定時間ごとにRoutineを繰り返し実行する
+            コンストラクタ
+                Repeat () = delete;
+                Repeat ( Repeat& ) = default;
+                Repeat ( Repeat&& ) = default;
+                Repeat ( Routine *routine, std::chrono::duration< long long, T > &interval )
+                Repeat ( Routine *routine, std::chrono::duration< long long, T > &&interval )
+                    引数
+                        routine
+                            繰り返し実行するRoutineのポインタ
+                        interval
+                            繰り返す間隔
+    */
     template < class T >
     class Repeat{
       public:
-        Repeat () = delete;
-        Repeat ( Repeat& ) = default;
-        Repeat ( Repeat&& ) = default;
-        Repeat ( Routine *routine, std::chrono::duration< long long, T > &interval ) : routine( routine ), interval( interval ){}
-        Repeat ( Routine *routine, std::chrono::duration< long long, T > &&interval ) : routine( routine ), interval( interval ){}
+        Repeat() noexcept = delete;
+        Repeat(Repeat &) noexcept = default;
+        Repeat(Repeat &&) noexcept = default;
+        Repeat(Routine *routine, std::chrono::duration<long long, T> &interval) noexcept : routine(routine), interval(interval) {}
+        Repeat(Routine *routine, std::chrono::duration<long long, T> &&interval) noexcept : routine(routine), interval(interval) {}
 
         virtual void start( void ) const;
       private:
