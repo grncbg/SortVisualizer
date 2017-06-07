9c96020315a972bc32cae68f4af0262948661d84
grncbg <grncbg@gmail.com>
2017-06-07 15:53:50 +0900

    Update コメントの追加

--- source/visualizer.cpp
+++ source/visualizer.cpp
@@ -15,9 +15,13 @@
 namespace kogcoder{
 
     int Visualizer::run() {
+        //新しい状態の取得
         int ret = setData();
+        //画面のクリア
         ncurses.clear();
+        //描画
         draw();
+        //画面の再描画
         ncurses.refresh();
         return ret;
     }
@@ -28,10 +32,13 @@ namespace kogcoder{
 
     int BubbleSortVisualizer::draw() const noexcept {
         for( unsigned int i = 0; i < data.size(); i++,ncurses.nextLine() ){
+            //ソート済み
             if( i < pickup.first )
                 ncurses.hbar( data[i], BLUE_LINE );
+            //変更
             else if( i == pickup.second || i == pickup.second + 1 )
                 ncurses.hbar( data[i], RED_LINE );
+            //それ以外
             else
                 ncurses.hbar( data[i] );
         }
