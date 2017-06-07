5bb7463fabccbf8b6cd8ce113060438efbe4f0a2
grncbg <grncbg@gmail.com>
2017-06-07 15:50:38 +0900

    Update コメント

--- include/time.hpp
+++ include/time.hpp
@@ -25,6 +25,20 @@ namespace kogcoder{
                 Time() noexcept = default
             デストラクタ
                 virtual ~Time() noexcept = default
+            テンプレート
+                class T
+                    waitで指定する待ち時間の接頭辞
+            関数
+              public:
+                template <class T>
+                void wait(const std::chrono::duration<long long, T>  &time) const noexcept;
+                void wait(const std::chrono::duration<long long, T> &&time) const noexcept;
+                    指定時間待機する
+                        引数
+                            time
+                                timeで指定した時間待機する
+                        戻り値
+                            なし
     */
     class Time{
       public:
@@ -32,22 +46,22 @@ namespace kogcoder{
         virtual ~Time() noexcept = default;
 
         template <class T>
-        void wait(const std::chrono::duration<long long, T> &) const noexcept;
-        template < class T >
-        void wait(const std::chrono::duration< long long, T > &&) const noexcept;
+        void wait(const std::chrono::duration<long long, T>  &) const noexcept;
+        template <class T>
+        void wait(const std::chrono::duration<long long, T> &&) const noexcept;
     };
 
     template < class T >
     void Time::wait(const std::chrono::duration< long long, T > &time) const noexcept {
-        std::chrono::steady_clock::time_point s,e;
+        std::chrono::steady_clock::time_point s, e;
         s = std::chrono::steady_clock::now();
         e = std::chrono::steady_clock::now();
         while( e - s < time )
             e = std::chrono::steady_clock::now();
     }
 
-    template < class T >
-    void Time::wait(const std::chrono::duration< long long, T > &&time) const noexcept {
+    template <class T>
+    void Time::wait(const std::chrono::duration<long long, T> &&time) const noexcept{
         wait(time);
     }
 
